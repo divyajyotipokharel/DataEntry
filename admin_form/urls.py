6e2fb75955961419d04a29aadb0b33c23623c813
Divyajyoti Pokharel <medivyajyoti@gmail.com>
2019-07-02 12:45:58 +0545

    edits

--- admin_form/urls.py
+++ admin_form/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from admin_form import views
 
 urlpatterns = [
-    path('',views.Home.as_view(), name = 'admin_home'),
+    path('',views.Home.as_view(), name = 'administrator_home'),
     path('profile/',views.AdminUserProfile.as_view(), name = 'admin_user_profile'),
     # path('operators/',views.UserPostList.as_view(), name = 'view_user_profile'),
     path('operator/',views.UserParam.as_view(), name = 'view_operator_profile'),
