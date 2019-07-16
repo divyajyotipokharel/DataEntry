97213397684d86d3760be11acb391118104b3fcb
Divyajyoti Pokharel <medivyajyoti@gmail.com>
2019-07-16 12:50:41 +0545

    update of download data

--- admin_form/urls.py
+++ admin_form/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',views.Home.as_view(), name = 'administrator_home'),
     path('profile/',views.AdminUserProfile.as_view(), name = 'admin_user_profile'),
     path('operator/',views.UserParam.as_view(), name = 'view_operator_profile'),
+    path('operator/download',views.UserParamDownload.download_view, name = 'data_download'),
 ]
--- admin_form/views.py
+++ admin_form/views.py
@@ -1,3 +1,9 @@
+import csv
+from io import StringIO
+from django.http import HttpResponse, StreamingHttpResponse
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from datetime import datetime, timedelta
 from datetime import date
 from django.db.models import Q
@@ -48,10 +54,62 @@ class AdminUserProfile(TemplateView):
 
 class UserParam(ListView):
     template_name = 'admin_form/u_post_list.html'
-    def get_queryset(self):
-        return Post.objects.filter(
-            Q(author = self.request.GET.get('q', ''))
-        ) 
+    def get_queryset(self, *args, **kwargs):
+        qs = Post.objects.all()
+        # print(self.request.GET)
+        query = self.request.GET.get('q', None)
+        if query is not None:
+            qs = qs.filter(author = self.request.GET.get('q',''))
+        return qs
+class Echo:
+    """An object that implements just the write method of the file-like
+    interface.
+    """
+    def write(self, value):
+        """Write the value by returning it, instead of storing in a buffer."""
+        return value
+
+class UserParamDownload(ListView):
+    template_name = 'admin_form/success_download.html'
+    def get_queryset(self, *args, **kwargs):
+        qs = Post.objects.all()
+        query = self.request.GET.get('q', None)
+        print(query)
+        contents =[]
+        qf = qs.filter(author = self.request.GET.get('q',''))
+        # with open('new_result.csv', 'w', encoding='utf-8') as csvfile:
+        #     for item in qf:
+        #         writer = csv.writer(csvfile)
+        #         writer.writerow([item.author,item.published_date,item.file_no,item.record_no,item.entry_date,item.ref_no,item.invoice_no,item.courier_name,item.con_no,item.dispatch_date,item.dispatch_by,item.sales_date,item.sales_time,item.customer_name,item.mail_address,item.agent_name,item.address,item.city,item.state,item.zip_code,item.customer_phone,item.credit_card_type,item.credit_card_number])
+        #         print(item.file_no,item.record_no)
+        if query is not None:
+            qs = qs.filter(author = self.request.GET.get('q',''))
+            # print(qs.values)
+        return (qs)
+    
+    def download_view(request, *args, **kwargs):
+        # Create the HttpResponse object with the appropriate CSV header.
+        
+        qs = Post.objects.all()
+        qf = qs.filter(author = request.GET.get('q',''))
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="user_data.csv"'
+        for item in qf:
+                writer = csv.writer(response)
+                writer.writerow([item.author,item.published_date,item.file_no,item.record_no,item.entry_date,item.ref_no,item.invoice_no,item.courier_name,item.con_no,item.dispatch_date,item.dispatch_by,item.sales_date,item.sales_time,item.customer_name,item.mail_address,item.agent_name,item.address,item.city,item.state,item.zip_code,item.customer_phone,item.credit_card_type,item.credit_card_number])
+
+        return response
+
+
+
+
+# def display_data(self):
+#     # templet_name = 'admin_form/u_post_list.html'
+#     return redirect("admin_user_profile",str(print('how are you?')))
+    
+    
+    
 
 
 # class AdminUserData(ListView):
